53ed6b009ff1b830a99fbc88a027b4675e43dacc
เอ็ก <136637894+natthawutphet@users.noreply.github.com>
2024-03-06 20:47:30 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/script';
 import Nav from "./Nav";
 import Footer from "./Footer";
+
 
 
 
@@ -9,44 +9,51 @@ export default function Document() {
 
 
 
-const urls = "https://www.service-ads.com";
-
   return (
     <Html lang="en">
     
     <Head>
+  
    
    <meta name="google-site-verification" content="g_4hsj1TVTqA-qt5kENBFgo24afTX6HFTUUoJw-K2v0" />      
 
 <meta name="keywords" content="ยิงads,facebook,สายเทา,โฆษณา,ยิงads facebook สายเทา,รับยิงads,รับยิงแอด สายเทา,facebook ads,google ads,google,รับยิงแอด,ads,รับทำโฆษณา,รับโฆษณา,Facebook,การตลาด,โฆษณาออนไลน์,เว็บไซต์,ตลาดเป้าหมาย,โฆษณาบนโซเชียลมีเดีย,Google Ads,การโฆษณาบน Facebook,การโฆษณาออนไลน์บนสื่อต่าง ๆ,การตลาดออนไลน์,การโปรโมท,โฆษณา Facebook,การโฆษณา Google,การโฆษณาสินค้า,การโฆษณาโปรโมชั่น,วิธีการโฆษณา,ความสำเร็จในการตลาด,บริการโฆษณา,การตลาดออนไลน์บน Facebook,การโฆษณาบนเว็บ,การวางแผนโฆษณา,รับจ้างโฆษณา" />
 <meta name="keywords" content="รับทำ seo สายเทา, รับทำ seo สายดำ, แอดสายเทา, รับยิงแอดสายเทา, กูเกิ้ลแอดสายเทา, รับทํา google ads, สายเทา seo สายเทา, รับทำโฆษณาสายเทา, ทำการตลาดสายเทา, ads สายเทา, รับทํา seo สายเทา, ยิงแอดสายเทา, โปรโมทสายเทา, facebook ads, สายเทา, การตลาดสายเทา, ยิงadsสายเทา, ยิงแอด google สายเทา, รับทำการตลาดสายเทา, รับจ้างยิงแอดสายเทา, โฆษณาสายเทา, ตลาดสายเทา, ads, สายเทา, แอดสายเทา, ยิงแอดสายเทา, pantip ยิงแอดสายเทาfacebook, ทําseoสายเทา, ยิงadsfacebookสายเทา, ยิงแอดสายเทาเฮียบอย, รับทำเว็บสายเทา, การทำseoสายเทา, ทำseoสายเทา, ยิงแอดyoutubeสายเทา, ยิงแอดสายเทาราคา, ยิงแอดสายเทา2022, ยิงแอดสายดำ, รับจ้างแอดสายเทา, ยิงแอดสายเทาgoogle, ตลาดสีเทา, แอดสีเทา, adsสีเทา, กราฟฟิกสายเทา, ยิงแอดเทา, รับยิงแอดfacebookสายเทา, รูปยิงแอดสายเทา, รับจ้างสายเทา, รับโฆษณาสายเทา " />
 
-<link rel="canonical" href="https://www.servermanagerads.com/" />
+<link rel="canonical" href="https://www.service-ads.com/" />
 <meta name="robots" content="index, follow" />
 <meta property="og:title" content="รับทำโฆษณาออนไลน์สายเทา" />
 <meta property="og:description" content="รับทำโฆษณาออนไลน์สายเทา ผู้เชี่ยวชาญด้านการตลาดบน Google, YouTube และ Facebook" />
-<meta property="og:url" content="https://www.servermanagerads.com/" />
+<meta property="og:url" content="https://www.service-ads.com/" />
 <meta property="og:type" content="website" />
 
-<meta name="twitter:card" content="https://www.servermanagerads.com/img/logo.png" />
+<meta name="twitter:card" content="https://www.service-ads.com/img/logo.ico" />
 
 
-<meta property="og:image" content="https://www.servermanagerads.com/img/logo.png" />
+<meta property="og:image" content="https://www.service-ads.com/img/logo.ico" />
 <meta property="og:image:width" content="1200" />
 <meta property="og:image:height" content="630" />
 <meta property="og:type" content="website" />
-<meta property="og:url" content={urls} />
+<meta property="og:url" content="https://www.service-ads.com/" />
 
-<meta name="twitter:card" content="https://www.servermanagerads.com/img/logo.png" />
-<meta name="twitter:image" content="https://www.servermanagerads.com/img/logo.png" />
+<meta name="twitter:card" content="https://www.service-ads.com/img/logo.ico" />
+<meta name="twitter:image" content="https://www.service-ads.com/img/logo.ico" />
 <meta name="twitter:title" content="Tiรับทำโฆษณาออนไลน์สายเทา" />
 <meta name="twitter:description" content="รับทำโฆษณาออนไลน์สายเทา ผู้เชี่ยวชาญด้านการตลาดบน Google, YouTube และ Facebook" />
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 
 <meta name="google-site-verification" content="PbQl1IGdzrIFiFnnE39varHoAFip5AIAk4QLxK-Nyto" />
- <meta property="og:image" content={`${urls}/favicon.ico`} />
+ <meta property="og:image" content="https://www.service-ads.com/logo.ico"/>
 
-
+ {(process.env.NODE_ENV === 'production') &&
+        <script dangerouslySetInnerHTML={{
+            __html: `(function (w, d, s, l, i) {
+                    w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+                    var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                    j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+                })(window, document, 'script', 'dataLayer', '	GTM-KGJWGD85');`
+        }}></script>
+    }
           
 
       </Head>
@@ -55,16 +62,12 @@ const urls = "https://www.service-ads.com";
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
       <Nav />
       <body>
-
+      {(process.env.NODE_ENV === 'production') &&
+        <noscript dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGJWGD85" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }}></noscript>
+    }
         
- 
-
-   
-
-
-
-        
-
 
 
      <Main />
