203ebdf98d48b13398e9134192c3ade8c5793049
เอ็ก <136637894+natthawutphet@users.noreply.github.com>
2024-03-06 14:35:52 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,15 @@ import '@/styles/style3.css';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
 import Head from 'next/head';
-
+import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
   
       <Component {...pageProps} />
-    
+     <GoogleTagManager gtmId="GTM-KGJWGD85" />
+      <GoogleAnalytics gaId="G-Z1NFV5NRKQ" />
     </>
   );
 }
