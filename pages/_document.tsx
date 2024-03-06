5f604b1161e930e207d8e4761eb720b8d11d21c3
เอ็ก <136637894+natthawutphet@users.noreply.github.com>
2024-03-06 20:49:43 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -51,7 +51,7 @@ export default function Document() {
                     w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
                     var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
                     j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-                })(window, document, 'script', 'dataLayer', '	GTM-KGJWGD85');`
+                })(window, document, 'script', 'dataLayer', '	GTM-5JBTTK9R');`
         }}></script>
     }
           
@@ -64,7 +64,7 @@ export default function Document() {
       <body>
       {(process.env.NODE_ENV === 'production') &&
         <noscript dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGJWGD85" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JBTTK9R" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
         }}></noscript>
     }
         
